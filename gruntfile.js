967d69a404d568bdc00456f587b449db0cd9147b
Paul Rodwell <paul.rodwell@btinternet.com>
2015-02-27 17:39:33 +0000

    Add license files, and script to extract contributors list.

--- gruntfile.js
+++ gruntfile.js
@@ -33,6 +33,24 @@ module.exports = function (grunt) {
     }
   });
 
+  grunt.registerTask( "update-authors", function () {
+    var getAuthors = require("grunt-git-authors"),
+    done = this.async();
+
+    getAuthors({
+      priorAuthors: grunt.config( "authors.prior")
+      }, function(error, authors) {
+        if (error) {
+          grunt.log.error(error);
+          return done(false);
+        }
+
+        grunt.file.write("AUTHORS.txt",
+          "Authors ordered by first contribution\n\n" +
+          authors.join("\n") + "\n");
+      });
+  });  
+
   grunt.registerTask('build', ['coffee', 'mochaTest']);
   grunt.registerTask('default', ['build']);
 
