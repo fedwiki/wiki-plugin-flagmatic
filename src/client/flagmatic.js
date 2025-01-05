f9509298fe53084bc1a317ab228b981ed6e59f26
Paul Rodwell <paul.rodwell@btinternet.com>
2025-01-05 15:01:59 +0000

    use builtin linear gradient

--- src/client/flagmatic.js
+++ src/client/flagmatic.js
@@ -9,58 +9,23 @@
 // The Flagmatic plugin offers a choice of new flags
 // item.choices selects how may, 40 by default
 
-const hsltorgb = (h, s, l) => {
-  h = (h % 360) / 360
-  const m2 = l * (s + 1)
-  const m1 = l * 2 - m2
-
-  const hue = num => {
-    let n = num
-    if (n < 0) {
-      n += 1
-    } else if (n > 1) {
-      n -= 1
-    }
-
-    if (n * 6 < 1) {
-      return m1 + (m2 - m1) * n * 6
-    } else if (n * 2 < 1) {
-      return m2
-    } else if (n * 3 < 2) {
-      return m1 + (m2 - m1) * (2 / 3 - n) * 6
-    } else return m1
-  }
-  return [hue(h + 1 / 3) * 255, hue(h) * 255, hue(h - 1 / 3) * 255]
-}
 const paint = canvas => {
   const ctx = canvas.getContext('2d')
-  const light = hsltorgb(Math.random() * 360, 0.78, 0.4)
-  const dark = hsltorgb(Math.random() * 360, 0.78, 0.55)
+  const light = `hsl(${Math.round(Math.random() * 360)}, 78%, 40%)`
+  const dark = `hsl(${Math.round(Math.random() * 360)}, 78%, 55%)`
   const angle = 2 * (Math.random() - 0.5)
-  const sin = Math.sin(angle)
-  const cos = Math.cos(angle)
-  const scale = Math.abs(sin) + Math.abs(cos)
+  const x2 = 31 * Math.cos(angle)
+  const y2 = 31 * Math.sin(angle)
 
-  const colprep = (col, p) => {
-    Math.floor(light[col] * p + dark[col] * (1 - p)) % 255
-  }
+  const gr = ctx.createLinearGradient(0, 0, x2, y2)
 
-  for (const x of [
-    ...Array(31)
-      .keys()
-      .map(i => i + 1),
-  ]) {
-    for (const y of [
-      ...Array(31)
-        .keys()
-        .map(i => i + 1),
-    ]) {
-      let p = sin >= 0 ? sin * x + cos * y : -sin * (31 - x) + cos * y
-      p = p / 31 / scale
-      ctx.fillStyle = `rgba(${colprep(0, p)}, ${colprep(1, p)}, ${colprep(2, p)}, 1)`
-      ctx.fillRect(x, y, 1, 1)
-    }
-  }
+  console.log('color', light, dark)
+
+  gr.addColorStop(0, light)
+  gr.addColorStop(1, dark)
+
+  ctx.fillStyle = gr
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
 }
 
 const delay = (msec, done) => {
